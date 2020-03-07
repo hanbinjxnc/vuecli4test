71eab27e4d8a26109154d3373ed6d1c9a06f9a65
hanbin <hanbinjxnc2009@126.com>
2020-03-07 17:46:49 +0800

    U: 新增返回按钮

--- src/components/elementui/dialog.vue
+++ src/components/elementui/dialog.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
         <el-form :model="form.name" :inline="true">
-          <el-form-item label="提示">
-              <el-button @click="dialogVisible = true" type="primary">点击打开 Dialog</el-button>
-              <el-dialog title="提示" :visible.sync="dialogVisible" width="30%" :before-close="handleClose">
-                  <span>这是一段信息</span>
-                  <span slot="footer" class="dialog-footer">
-                      <el-button type="" @click="dialogVisible = false">取消</el-button>
-                      <el-button type="primary" @click="dialogVisible = false">确定</el-button>
-                  </span>
-              </el-dialog>
-          </el-form-item>
+            <el-form-item label="">
+                <el-button type="primary" @click="goback()">返回</el-button>
+            </el-form-item>
+            <el-form-item label="提示">
+                <el-button @click="dialogVisible = true" type="primary">点击打开 Dialog</el-button>
+                <el-dialog title="提示" :visible.sync="dialogVisible" width="30%" :before-close="handleClose">
+                    <span>这是一段信息</span>
+                    <span slot="footer" class="dialog-footer">
+                        <el-button type="" @click="dialogVisible = false">取消</el-button>
+                        <el-button type="primary" @click="dialogVisible = false">确定</el-button>
+                    </span>
+                </el-dialog>
+            </el-form-item>
         </el-form>
     </div>
 </template>
@@ -25,6 +28,10 @@ export default {
     methods:{
         handleClose(done){
             this.$confirm('确定关闭？').then(_ => { done(_) }).catch(_ => { done(_)})
+        },
+        goback(){
+            // this.$router.go(-1)
+            this.$router.back()
         }
     }
 }
